6fb001e27233c5b016d465c4a31e31105332958b
Aaditya Gautam <136166563+aadiG17@users.noreply.github.com>
2025-07-24 09:10:48 +0000

    Coversion of types

---
+++ 01-basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+
+
+// "33" => 33
+// "333sab" => NaN
+// true => 1: false => 0
+
+
+let isLoggedIn = ""
+
+let  booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false
+// "jcdhc" => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
