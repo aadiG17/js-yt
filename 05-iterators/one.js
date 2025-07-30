c81dbf4776da2b62c0fb25a7e34cf0f0f58535e0
Aaditya Gautam <136166563+aadiG17@users.noreply.github.com>
2025-07-30 04:12:47 +0000

    loops in js

---
+++ 05-iterators/one.js
@@ -0,0 +1,30 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+    
+    
+// }
+
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+// console.log(`Value of i is ${index}`);
+// }
+
+// for (let index = 1; index < 20; index++) {
+//     if(index == 5){
+//         console.log(`Detected 5`);
+//         break;
+//     }    
+//     console.log(`Value of i is ${index}`);
+// }
+
+// for (let index = 1; index < 20; index++) {
+//     if(index == 5){
+//         console.log(`Detected 5`);
+//         continue;
+//     }    
+//     console.log(`Value of i is ${index}`);
+// }
+
+
